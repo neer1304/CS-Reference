43bc90be60cd0298688bb5e81a1ee676ead97021
Neeraj Sharma <neer.1304@gmail.com>
2015-02-11 19:31:26 +0530

    Update tok_cat_header.h

--- C Adv/Dynamic_Memory/Tok_Cat/include/tok_cat_header.h
+++ C Adv/Dynamic_Memory/Tok_Cat/include/tok_cat_header.h
@@ -4,13 +4,6 @@
 *
 *  DESCRIPTION  : Contains header files and function prototypes string 
 *		  tokenization and reverse concatenation program
-*
-*  DATE      NAME    REFERENCE          REASON
-*
-*  1-JUN-10  Sibu    PRISM-3 C FG       Initial Creation.
-*
-*  Copyright 2010, Aricent Technologies (Holdings) Ltd
-*
 **************************************************************************/
 
 /* Avoiding multiple inclusions of header file */
